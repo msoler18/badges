a8b5cf95fcceb5d57f5c4f060cd4cd8d11b77f1c
Miguel Soler <msoler735@gmail.com>
2020-03-25 18:47:55 -0500

    Create main page for badges

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
 
-import Badge from './components/Badge'
+import BadgeNew from './pages/BagdeNew'
 
 
 const container = document.getElementById('app');
 
-ReactDOM.render(
-  <Badge 
-  firstName="Miguel" 
-  lastName="Soler" 
-  jobTitle="WebDev"
-  twitter="msoler18"
-  />, 
-  container
-);
+ReactDOM.render(<BadgeNew/>,container);
